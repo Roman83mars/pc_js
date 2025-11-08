5b0bae5f87663be4df39d59550b688044b380cc9
Roman83mars <roman83mars@gmail.com>
2025-11-09 02:22:20 +0300

    lesson01 complete

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const contents = document.querySelectorAll('.program-line__content')
+
+contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title')
+    const descr = elem.querySelector('.program-line__descr')
+
+    title.addEventListener('click', () => {
+        if (descr.classList.contains('active')) {
+            descr.classList.remove('active')
+        } else {
+            contents.forEach((otherElem) => {
+                const otherDescr = otherElem.querySelector('.program-line__descr')
+                otherDescr.classList.remove('active')
+            })
+            descr.classList.add('active')
+        }
+    })
+})
