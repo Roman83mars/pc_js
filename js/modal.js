aed8195f41b56213633cf1fe401ba87637418f7a
Roman83mars <roman83mars@gmail.com>
2025-11-09 22:44:10 +0300

    lesson02 complete

---
+++ js/modal.js
@@ -0,0 +1,32 @@
+const modalBtn = document.querySelectorAll('.modal__button, .course__button')
+const modal = document.querySelector('.modal')
+const modalInner = modal.querySelector('.modal__inner')
+
+const closeBtn = document.createElement('img')
+modalInner.style.position = 'relative'
+closeBtn.src = './img/close.svg'
+closeBtn.style.width = '5rem'
+closeBtn.style.height = '5rem'
+closeBtn.style.cursor = 'pointer'
+closeBtn.style.position = 'absolute'
+closeBtn.style.top = '2rem'
+closeBtn.style.right = '2rem'
+modalInner.prepend(closeBtn)
+
+
+modalBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modal.style.display = 'flex'
+    })
+})
+
+modal.addEventListener('click', (e) => {
+    const modalContent = e.target.closest('.modal__inner')
+    if (!modalContent) {
+        modal.style.display = ''
+    }
+})
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = ''
+})
